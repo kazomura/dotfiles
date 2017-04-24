27299d1122dc76b1d15043c44afaff1c28a767ff
kazomura <kazomur+git@gmail.com>
2017-04-24 22:09:23 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -7,34 +7,35 @@ done
 source $HOME/.zshenv
 
 echo "brew install..."
+brew update
 brew file install
 
-if [ ! -f "$HOME/.go/bin/ghq" ]; then
+if [ ! -f "$HOME/go/bin/ghq" ]; then
   echo "install ghq..."
   go get github.com/motemen/ghq
-  GHQ_ROOT="$(ghq root)"
 fi
+GHQ_ROOT="$($HOME/go/bin/ghq root)"
 
-if [ ! -f "$HOME/.go/bin/peco" ]; then
+if [ ! -f "$HOME/go/bin/peco" ]; then
   echo "install peco..."
   go get github.com/peco/peco/cmd/peco
 fi
 
 if [ ! -d "$GHQ_ROOT/github.com/zplug/zplug" ]; then
   echo "install zplug..."
-  $HOME/.go/bin/ghq get git://github.com/zplug/zplug
+  $HOME/go/bin/ghq get git://github.com/zplug/zplug
 fi
 
 if [ ! -d "$GHQ_ROOT/github.com/riywo/anyenv" ]; then
   echo "install anyenv..."
-  $HOME/.go/bin/ghq get git://github.com/riywo/anyenv
+  $HOME/go/bin/ghq get git://github.com/riywo/anyenv
 fi
 ln -snfv "$GHQ_ROOT/github.com/riywo/anyenv" "$HOME/.anyenv"
 ANYENV_ROOT=$HOME/.anyenv
 
 if [ ! -d "$ANYENV_ROOT/envs/rbenv" ]; then
   echo "install rbenv..."
-  $ANYENV_ROOT/bin/anyenv install rbenv
+  $ANYENV_ROOT/bin/s
   echo "install ruby..."
   rbenv install 2.4.1
   rbenv global 2.4.1
